13d92987e3ad382ef916b8532a418e09600e6901
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-11-25 14:32:02 -0300

    Initial Commit

---
+++ src/main/java/com/example/whoInbiblie/WhoInbiblieApplication.java
@@ -0,0 +1,13 @@
+package com.example.whoInbiblie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoInbiblieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoInbiblieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whoInbiblie/WhoInbiblieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whoInbiblie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoInbiblieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
